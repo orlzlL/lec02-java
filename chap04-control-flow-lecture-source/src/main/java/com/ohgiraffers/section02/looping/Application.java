e978399c45f0598835f11a3f557c906516dd4304
yerincho06 <yerincho06@gmail.com>
2024-01-15 15:34:39 +0900

    array

---
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,47 @@
+package com.ohgiraffers.section02.looping;
+public class Application {
+    public static void main(String[] args) {
+
+        A_for aClass = new A_for();
+
+        /* 필기. 단독 for문 흐름 확인용 메소드 호출 */
+//        aClass.testSimpleForStatement();
+
+        /* 필기. 반복문을 사용하는 예제(반복문의 필요성) */
+//        aClass.testForExample();
+
+        B_nestedFor bClass = new B_nestedFor();
+
+        /* 필기. 중첩 반복문을 사용하는 예제 */
+//        bClass.printGugudanFromTwoToNime();
+
+        /* 필기. 중첩 반복문을 사용하는 예제 */
+//        bClass.printUpgradeGugudanFromTwoToNine();
+
+        /* 필기. 중첩 반복문을 활용한 별찍기 관련 예제 */
+//        bClass.printStars();
+
+        C_while cClass = new C_while();
+
+        /* 필기. 단독 while문 흐름 확인용 메소드 호출 */
+//        cClass.testSimpleWhileStatement();
+
+        /* 필기. while문 사용하는 예제 */
+//        cClass.testWhileExample();
+
+        D_dowhile dClass = new D_dowhile();
+
+        /* 필기. 단독 do-while문 흐름 확인용 메소드 호출 */
+//        dClass.testSimpleDoWhileStatement();
+
+        /* 필기. do-while 문 사용하는 예제 */
+        dClass.testDoWhileExample();
+
+
+
+
+
+
+    }
+}
+
