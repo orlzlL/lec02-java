9f28cee922a1a35aeb55e064d454436ca219e72d
yerincho06 <yerincho06@gmail.com>
2024-01-18 12:44:04 +0900

    polymorphism

---
+++ chap08-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/polymorphism/Application.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section01.polymorphism;
+
+public class Application {
+    public static void main(String[] args) {
+
+        /* 수업목표. 다형성과 타입 형변환에 대해 이해할 수 있다. */
+        Animal animal = new Animal();
+        animal.eat();
+        animal.run();
+        animal.cry();
+
+        Tiger tiger = new Tiger();
+        tiger.eat();
+        tiger.run();
+        tiger.cry();
+        tiger.bite();
+
+        Rabbit rabbit = new Rabbit();
+        rabbit.eat();
+        rabbit.run();
+        rabbit.cry();
+        rabbit.jump();
+    }
+}
