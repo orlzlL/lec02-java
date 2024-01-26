6b4cb891ba817c6fca450a04939571116932c2da
yerincho06 <yerincho06@gmail.com>
2024-01-26 14:41:24 +0900

    stream

---
+++ chap15-lambda-lecture-source/src/main/java/com/ohgiraffers/section01/intro/OuterCalculator.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.section01.intro;
+
+public interface OuterCalculator {
+
+    @FunctionalInterface
+    public interface Sum {
+        int sumTwoNumber(int a, int b);
+    }
+
+    @FunctionalInterface
+    public interface Minus {
+        int minusTwoNumber(int a, int b);
+    }
+
+    @FunctionalInterface
+    public interface Multiple {
+        int multiplyNumber(int a, int b);
+    }
+
+    @FunctionalInterface
+    public interface Divide {
+        int divideTwoNumber(int a, int b);
+    }
+
+
+}
